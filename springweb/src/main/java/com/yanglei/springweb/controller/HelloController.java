3c5151bc2d1991a9ba085f6b4f621be51958b57b
yanglei <1269123897@qq.com>
2020-12-06 15:49:40 +0800

    springweb commit

---
+++ springweb/src/main/java/com/yanglei/springweb/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.yanglei.springweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello(){
+        return "helloWorld";
+    }
+}
