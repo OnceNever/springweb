31196654a78c29070dc95a4cf1cdc895a45e72db
yanglei <1269123897@qq.com>
2020-12-07 22:06:31 +0800

    20201207 22：06 commit

---
+++ springweb/src/main/java/com/yanglei/springweb/config/MyMvcConfiguration.java
@@ -0,0 +1,18 @@
+package com.yanglei.springweb.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//如果我们要扩展springmvc，官方建议我们这样去做
+@Configuration
+public class MyMvcConfiguration implements WebMvcConfigurer {
+
+    //视图跳转
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/yang").setViewName("test");
+    }
+}
+
+
