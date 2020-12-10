519d45ce5a7abb83d44b6b5954cd3513d236b487
yanglei <1269123897@qq.com>
2020-12-10 21:53:42 +0800

    20201210 21：53 commit

--- springweb/src/main/java/com/yanglei/springweb/config/MyMvcConfiguration.java
+++ springweb/src/main/java/com/yanglei/springweb/config/MyMvcConfiguration.java
@@ -3,6 +3,7 @@ package com.yanglei.springweb.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,7 +16,9 @@ public class MyMvcConfiguration implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/login").setViewName("index");
         registry.addViewController("/index.html").setViewName("index");
+        registry.addViewController("/main.html").setViewName("dashboard");
     }
 
     //自定义国际化组件
@@ -23,6 +26,14 @@ public class MyMvcConfiguration implements WebMvcConfigurer {
     public LocaleResolver localeResolver(){
         return new MyLocaleResolver();
     }
+
+    /**
+     * 自定义登录拦截器
+     * @param registry
+     */
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(new LoginHandlerInterceptor()).
+                addPathPatterns("/**").
+                excludePathPatterns("/index", "/index.html", "/login", "/css/**", "/js/**", "/img/**");
+    }
 }
-
-
---
+++ springweb/src/main/java/com/yanglei/springweb/controller/LoginController.java
@@ -0,0 +1,25 @@
+package com.yanglei.springweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/login")
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password,
+                        Model model, HttpSession session){
+        if(null != username && password != null){
+            session.setAttribute("loginUser", username);
+            return "redirect:main.html";
+        } else {
+            //告诉用户登陆失败
+            model.addAttribute("msg", "用户名或密码错误！");
+            return "index";
+        }
+    }
+}
