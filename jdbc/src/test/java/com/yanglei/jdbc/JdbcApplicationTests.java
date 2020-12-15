fe6bca4b5869829909f34b2cadc4d01e4c7d2cd2
yanglei <1269123897@qq.com>
2020-12-15 21:47:44 +0800

    2020-12-15 21:47 commit

--- jdbc/src/test/java/com/yanglei/jdbc/JdbcApplicationTests.java
+++ jdbc/src/test/java/com/yanglei/jdbc/JdbcApplicationTests.java
@@ -1,13 +1,27 @@
 package com.yanglei.jdbc;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootTest
 class JdbcApplicationTests {
 
+    @Autowired
+    DataSource dataSource;
     @Test
-    void contextLoads() {
+    void contextLoads() throws SQLException {
+        //查看默认数据源
+        System.out.println(dataSource.getClass());
+
+        //获取链接
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
     }
 
 }
