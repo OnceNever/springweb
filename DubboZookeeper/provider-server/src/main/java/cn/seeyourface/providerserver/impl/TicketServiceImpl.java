40d966cee58053a590138b500a6d33af6c11d230
yanglei <1269123897@qq.com>
2021-03-07 19:02:07 +0800

    zookeeper+dubbo commit

--- DubboZookeeper/provider-server/src/main/java/cn/seeyourface/providerserver/impl/TicketServiceImpl.java
+++ DubboZookeeper/provider-server/src/main/java/cn/seeyourface/providerserver/impl/TicketServiceImpl.java
@@ -1,7 +1,11 @@
 package cn.seeyourface.providerserver.impl;
 
 import cn.seeyourface.providerserver.services.TicketService;
+import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.stereotype.Service;
 
+@DubboService //一启动就能被扫描到，自动注册到注册中心
+@Service
 public class TicketServiceImpl implements TicketService {
     @Override
     public String getTicket() {
